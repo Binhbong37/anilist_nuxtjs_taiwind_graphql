cd617989264ff29ec13252069fd69e72a096de16
Hoang Van Binh <binhhvFX12117@funix.edu.vn>
2022-04-27 12:00:52 +0700

    1. Khởi tạo các file, thêm được tailwind, lấy được dữ liệu từ anilist

---
+++ pages/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="p-8">
+    <h1 class="text-center font-bold">Hello word</h1>
+    <div>
+      <img :src="Media.bannerImage" alt="Anime" />
+      <h2 class="p-35">{{ Media.title.english }}</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+import gql from "graphql-tag";
+
+const query = gql`
+  query {
+    Media(type: ANIME) {
+      bannerImage
+      id
+      title {
+        english
+        native
+        romaji
+      }
+    }
+  }
+`;
+
+export default {
+  apollo: {
+    Media: {
+      query: query,
+    },
+  },
+  data() {
+    return {
+      test: "ABC",
+    };
+  },
+};
+</script>
